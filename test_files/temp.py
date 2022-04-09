9ac5e5ac3cfa4b7e2507ff9bc9f95fa3d9fe2802
Jamie <75698437+jcod2@users.noreply.github.com>
2022-04-09 11:03:19 +0100

    Delete test_files directory

--- test_files/temp.py
+++
@@ -1,9 +0,0 @@
-@app.route("/national_dex/<string:region>")
-def contents(region):
-    db = get_db()
-    pokemon = db.execute("""SELECT * FROM pokemon 
-                         WHERE region_of_origin = ?
-                         ORDER BY pokedex_number ASC;""", (region, )).fetchall()
-    close_db()
-    return render_template("poke_pages/contents.html", 
-        pokemon=pokemon, title=region + "'s Regional Pokémon")
